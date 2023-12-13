b10d39f558f01cb0b6e0e48e8d5e6b5797398bea
saiganesh-sristla <saiganesh3108@gmail.com>
2023-12-13 19:20:55 +0530

    added next smaller element

---
+++ nse.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+vector<int> nge(vector<int> input, int n)
+{
+    vector<int> output(n, -1);
+    stack<int> st;
+
+    st.push(0);
+    for (int i = 1; i < n; i++)
+    {
+        while ((!st.empty()) && input[i] < input[st.top()])
+        {
+            output[st.top()] = input[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+    return output;
+}
+
+int main()
+{
+    vector<int> v = {2, 5, 2, 4, 2, 6, 4, 1};
+    int n = v.size();
+
+    vector<int> result = nge(v, n);
+
+    cout << "Next smaller Elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << result[i] << " ";
+    }
+
+    return 0;
+}
